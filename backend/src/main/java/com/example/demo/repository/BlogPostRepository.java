86be5650a7ad2ef6e8ac6d77c3799f0a0e6f974f
Jennifer Traeger <jgoodnight7272@gmail.com>
2025-09-05 15:45:46 -0700

    Persist data for testing

--- backend/src/main/java/com/example/demo/repository/BlogPostRepository.java
+++ backend/src/main/java/com/example/demo/repository/BlogPostRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.BlogPost;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+    @Query("SELECT b FROM BlogPost b ORDER BY b.date DESC")
+    List<BlogPost> findAllOrderByDateDesc();
 }
